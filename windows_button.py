79c69f57b08f482bd810f055dc504330f7bf4706
Riwaj Dangol <riwaj.dangol@patancollege.edu.np>
2026-02-12 09:01:15 +0545

    Minute spec changes

--- windows_button.py
+++ windows_button.py
@@ -1,16 +1,13 @@
 import subprocess
 
-
 def open_advanced_display():
     try:
         subprocess.Popen("start ms-settings:display", shell=True)
     except Exception as e:
         print(f"Error: {e}")
 
-
 def open_hags_settings():
     subprocess.Popen("start ms-settings:display-advancedgraphics", shell=True)
-
 
 def open_gamebar_settings():
     subprocess.Popen("start ms-settings:gaming-gamemode", shell=True)
