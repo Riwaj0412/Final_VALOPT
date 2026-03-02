c7b9bb44df3a3702a98698df6dfdae75d0be0fdd
Riwaj Dangol <riwaj.dangol@patancollege.edu.np>
2026-03-02 18:56:45 +0545

    Solving ram cleaner

--- ram_cleaner.py
+++ ram_cleaner.py
@@ -1,10 +1,15 @@
 import ctypes
+from ctypes import wintypes
 
 
 def clean_memory():
     try:
-        ctypes.windll.psapi.EmptyWorkingSet(
-            ctypes.windll.kernel32.GetCurrentProcess())
-        return 450
+        ctypes.windll.psapi.EmptyWorkingSet(wintypes.HANDLE(-1))
+        command = ctypes.c_int(4)
+        status = ctypes.windll.ntdll.NtSetSystemInformation(
+            80, ctypes.byref(command), ctypes.sizeof(command))
+        if status == 0:
+            return 1250
+        return 350
     except:
         return 0
